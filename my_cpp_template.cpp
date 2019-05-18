3dc555d48b8b526cd6c5a525c4e06c569d5da4fe
Burak Coskun <burakcosk@gmail.com>
2019-05-18 12:04:50 +0200

    Use bits/stdc++ in cpp template instead of individual imports

--- my_cpp_template.cpp
+++ my_cpp_template.cpp
@@ -1,15 +1,4 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
