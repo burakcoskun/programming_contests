ce7a0f2feb4ba925f337f494e879981d89304239
Burak Coskun <burakcosk@gmail.com>
2019-08-18 15:34:55 +0200

    Remove shorthand defines from  cpp template
    Since I know use clang autocomplete I don't need shorthand definitions
    for common operations.

--- my_cpp_template.cpp
+++ my_cpp_template.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 typedef long long ll;
 
-#define pb push_back
-#define mp make_pair
-#define fs first
-#define sc second
-
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
